ce965d374ab1ebbadbe6aa582da6524329078468
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-05-04 23:59:15 +0100

    Modified config files
    - Modified premake
    - Modified .gitignore

--- premake5.lua
+++ premake5.lua
@@ -7,14 +7,13 @@ project "ImGui"
 
 	files
 	{
-        "backends/**.h",
-        "backends/**.cpp",
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
 		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
